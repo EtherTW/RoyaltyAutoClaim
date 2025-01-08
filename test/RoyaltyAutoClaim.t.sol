eb99c15fa7737c2cd3b902707da6c6fdfc863c22
Johnson Chen <chnejohnson@gmail.com>
2025-01-08 16:05:01 +0800

    chore: update readme

---
+++ test/RoyaltyAutoClaim.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "forge-std/Test.sol";
+
+contract RoyaltyAutoClaimTest is Test {
+    function setUp() public {}
+
+    function test() public {}
+}
