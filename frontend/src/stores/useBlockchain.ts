b36f300e08ce199db328ef8f8e2068591d820a9e
Johnson Chen <chnejohnson@gmail.com>
2025-02-13 11:17:00 +0800

    dev: move blockchain config to @/config

--- frontend/src/stores/useBlockchain.ts
+++ frontend/src/stores/useBlockchain.ts
@@ -1,34 +1,7 @@
 import { JsonRpcProvider } from 'ethers'
 import { defineStore } from 'pinia'
 import { PimlicoBundler } from 'sendop'
-
-export const ALCHEMY_API_KEY = import.meta.env.VITE_ALCHEMY_API_KEY
-
-if (!ALCHEMY_API_KEY) {
-	throw new Error('ALCHEMY_API_KEY is not set')
-}
-
-enum CHAIN_ID {
-	LOCAL = '1337',
-	SEPOLIA = '11155111',
-}
-
-const DEFAULT_CHAIN_ID = CHAIN_ID.LOCAL
-
-const RPC_URL: { [key: string]: string } = {
-	[CHAIN_ID.LOCAL]: `http://localhost:8545`,
-	[CHAIN_ID.SEPOLIA]: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-}
-
-const EXPLORER_URL: { [key: string]: string } = {
-	[CHAIN_ID.LOCAL]: '',
-	[CHAIN_ID.SEPOLIA]: 'https://scope.sh/11155111',
-}
-
-const BUNDLER_URL: { [key: string]: string } = {
-	[CHAIN_ID.LOCAL]: 'http://localhost:4337',
-	[CHAIN_ID.SEPOLIA]: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-}
+import { CHAIN_ID, DEFAULT_CHAIN_ID, RPC_URL, EXPLORER_URL, BUNDLER_URL } from '@/config'
 
 export const useBlockchainStore = defineStore('useBlockchainStore', () => {
 	const chainId = ref<CHAIN_ID>(DEFAULT_CHAIN_ID)
